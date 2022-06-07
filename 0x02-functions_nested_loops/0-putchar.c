4b25ddefbc0cbc9b870aab8310ef4dc6d847f576
h3lenn <hellentovz@gmail.com>
2022-06-07 11:15:09 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints putchar as a message
+ * prints putchar as a message
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int str[] = {45, 112, 117, 116 ,99, 104, 97, 114};
 	int count, sz;
 
 	sz = sizeof(str) / sizeof(int);
